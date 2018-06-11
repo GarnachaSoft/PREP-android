0eebf5bc7929d5e84de2f8873754f74ea52fb3de
Guillermo Canales <memoadian@gmail.com>
2018-06-11 11:36:48 -0500

    cambio de status

--- app/src/main/java/org/prepciudadano/prepciudadano/NewPhotoActivity.kt
+++ app/src/main/java/org/prepciudadano/prepciudadano/NewPhotoActivity.kt
@@ -198,7 +198,7 @@ class NewPhotoActivity : AppCompatActivity() {
         val ref = FirebaseDatabase.getInstance().getReference("templates")
         val templateId:String = ref.push().key!!
 
-        val template = Template(templateId, boxId, urlImage, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        val template = Template(templateId, boxId, urlImage, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
         ref.child(templateId).setValue(template).addOnCompleteListener {
             val intent = Intent(this, FormResultsActivity::class.java)
             intent.putExtra("id", templateId)
